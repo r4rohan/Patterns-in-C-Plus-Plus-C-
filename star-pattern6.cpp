667a0db1a958b88c24e44cdf349a6b6220bb7e13
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2019-03-14 00:54:53 +0530

    Star Pattern
    This code will print
        *
       * *
      * * *
     * * * *
    * * * * *

---
+++ star-pattern6.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// i is for rows and j is for columns and k for toggle
+    int i , j, k;
+    int n; //for entering no of rows
+    cin >> n;
+    for ( i = 1; i <= n; i++ )
+    {
+    	k = 1;
+        for( j = 1; j <= 2 * n - 1; j++ )
+        // in order to print it i must be great or equal to j
+        if( j >= (n + 1) - i  && j <= (n - 1) + i && k)
+        {
+            cout << "*";
+            k = 0;
+        }
+        else
+        {
+        	cout << " ";
+        	k = 1;
+		}
+		cout << endl;
+    }
+}
