ee7a34a39473768952b0a8367fb2c30f5f1cb46d
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2019-03-15 03:20:13 +0530

    Star Pattern
    This code will print:
                 A
              A B A
          A B C B A
       A B C D C B A
    A B C D E D C B A

---
+++ star-pattern12.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// i is for rows and j is for columns and k for toggle
+    int i , j;
+	char k;
+    int n; //for entering no of rows
+    cin >> n;
+    for ( i = 1; i <= n; i++ )
+    {
+    	k = 'A';
+        for( j = 1; j <= 2 * n - 1; j++ )
+        if( j >= (n + 1) - i  && j <= (n - 1) + i)   
+        {
+            cout << k;
+            j<n?k++:k--;    
+        }
+        else
+        {
+        	cout << " ";
+		}
+		cout << endl;
+    }
+}
