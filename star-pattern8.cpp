fc2c98456d78468f668c878e827e1db64c29b275
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2019-03-15 02:58:40 +0530

    Star Pattern
    This code will print
         1
       123
      12345
     1234567
    123456789

---
+++ star-pattern8.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// i is for rows and j is for columns and k for toggle
+    int i , j, k;
+    int n; //for entering no of rows
+    cin >> n;
+    for ( i = 1; i <= n; i++ )
+    {
+    	k = 1;
+        for( j = 1; j <= 2 * n - 1; j++ )
+        if( j >= (n + 1) - i  && j <= (n - 1) + i)   
+        {
+            cout << k;
+            k++;     
+        }
+        else
+        {
+        	cout << " ";
+		}
+		cout << endl;
+    }
+}
